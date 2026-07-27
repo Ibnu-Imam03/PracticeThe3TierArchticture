547d54e4cfd1236a4a64625eab02b5ef9d052776
Abubakar Yimam <abubakaryimam37@gmail.com>
2026-07-27 05:34:24 -0700

    Add testAddNewContact() method and update Main usage
    Added static method testAddNewContact() in Program.cs to create and save a new contact, printing its ID on success. Replaced testFindContact(1) call in Main with testAddNewContact().

--- Program.cs
+++ Program.cs
@@ -29,11 +29,30 @@ namespace ContactConsolApp
 
         }
 
+        static void testAddNewContact()
+        {
+            clsContact Contact1 = new clsContact();
+            Contact1.FirstName = "Fadi";
+            Contact1.LastName = "Maher";
+            Contact1.Email = "A@a.com";
+            Contact1.Phone = "010010";
+            Contact1.Address = "address1";
+            Contact1.DateOfBirth = new DateTime(1977, 11, 6, 10, 30, 0);
+            Contact1.CountryID = 1;
+            Contact1.ImagePath = "";
+
+            if (Contact1.Save())
+            {
+                Console.WriteLine("Contact Added Successfully with id=" + Contact1.ID);
+
+            }
+        }
+
         static void Main(string[] args)
         {
 
-            testFindContact(1);
-            //testAddNewContact();
+            //testFindContact(1);
+            testAddNewContact();
             //testUpdateContact(1);
             //ListContacts();
 
