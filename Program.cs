68e472bf1ddcc168c09ec4408f7a525b6f2cdf26
Abubakar Yimam <abubakaryimam37@gmail.com>
2026-07-27 03:57:56 -0700

    Update .gitignore, add SqlClient, and refactor Program.cs
    - Added /bin/Debug/net10.0 to .gitignore to exclude build output.
    - Updated ContactsConsolApp-Presention.csproj to reference Microsoft.Data.SqlClient v7.0.2.
    - Included .gitignore in Solution Items in PracticeThe3TierArchticture.slnx.
    - Refactored Program.cs: added namespace, Program class, testFindContact method, and updated Main to display contact details for ID 1.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,44 @@
 ﻿
+using ContactBusinessLayer;
 using System;
 using System.Data;
-using ContactBusinessLayer;
+using System.Diagnostics.Contracts;
+
+namespace ContactConsolApp
+{
+    internal class Program
+    {
+        static void testFindContact(int ID)
+        {
+            clsContact Contact1 =  clsContact.Find(ID);
+
+            if (Contact1 != null)
+            {
+                Console.WriteLine(Contact1.FirstName + " " + Contact1.LastName);
+                Console.WriteLine(Contact1.Email);
+                Console.WriteLine(Contact1.Phone);
+                Console.WriteLine(Contact1.Address);
+                Console.WriteLine(Contact1.DateOfBirth);
+                Console.WriteLine(Contact1.CountryID);
+                Console.WriteLine(Contact1.ImagePath);
+            }
+            else
+            {
+                Console.WriteLine("Contact [" + ID + "] Not found!");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            testFindContact(1);
+            //testAddNewContact();
+            //testUpdateContact(1);
+            //ListContacts();
+
+            Console.ReadKey();
+
+        }
+    }
+}
